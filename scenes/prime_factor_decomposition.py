c8204a511cee2fee0474f1365e7af696def5b2c7
Makcime <marlier.maxime@gmail.com>
2024-10-20 17:29:59 +0200

    WIP adding a second decompostion

--- scenes/prime_factor_decomposition.py
+++ scenes/prime_factor_decomposition.py
@@ -6,47 +6,68 @@ from manim import *
 class PrimeFactorDecomposition(Scene):
     def construct(self):
         # List of primes under 20
-        primes = [2, 3, 5, 7, 11, 13, 17, 19]
-        primes_texts = [MathTex(str(p)) for p in primes]
+        self.primes = [2, 3, 5, 7, 11, 13, 17, 19]
+        primes_texts = [MathTex(str(p)) for p in self.primes]
 
         # Arrange primes horizontally
-        primes_group = VGroup(*primes_texts).arrange(RIGHT, buff=0.5)
+        self.primes_group = VGroup(*primes_texts).arrange(RIGHT, buff=0.5)
 
         # Primes label
-        primes_label = Text("Nombres premiers < 20:")
-        primes_label.to_edge(UP).shift(DOWN * 0.5)
+        self.primes_label = Text("Nombres premiers < 20:")
+        self.primes_label.to_edge(UP).shift(DOWN * 0.5)
 
         # Position primes group
-        primes_group.next_to(primes_label, DOWN, buff=0.3)
+        self.primes_group.next_to(self.primes_label, DOWN, buff=0.3)
 
         # Add label and primes to scene
-        self.play(Write(primes_label))
-        self.play(Write(primes_group))
+        self.play(Write(self.primes_label))
+        self.play(Write(self.primes_group))
+
+        # Decompose 126 and move it to the left
+        group_126 = self.decompose_number(126, LEFT * 4.8)
+
+        # Pause before starting the next decomposition
+        self.wait(1)
+
+        # Decompose 120
+        group_120 = self.decompose_number(120, RIGHT * 4)
+
+        self.wait(2)
+        # Optionally, you can adjust the positions or add more numbers
+
+    def decompose_number(self, number, shift_amount):
+        # Create a group to hold all elements related to this decomposition
+        decomposition_group = VGroup()
 
         # Vertical line
-        vertical_line = Line(start=primes_group.get_bottom() + DOWN * 0.5, end=DOWN * 2)
-        self.play(Create(vertical_line))
+        vertical_line = Line(
+            start=self.primes_group.get_bottom() + DOWN * 0.5, end=DOWN * 2
+        )
 
-        # Number 126 at the top
-        number_126 = MathTex("126")
-        number_126.next_to(vertical_line.get_top() + DOWN * 0.2, LEFT)
-        self.play(Write(number_126))
+        # Number at the top
+        number_tex = MathTex(str(number))
+        number_tex.next_to(vertical_line.get_top() + DOWN * 0.2, LEFT)
+
+        # Add vertical line and number to the group and scene
+        self.play(Create(vertical_line))
+        self.play(Write(number_tex))
+        decomposition_group.add(vertical_line, number_tex)
 
         # Initialize variables for the decomposition
-        current_number = 126
-        number_mobject = number_126
+        current_number = number
+        number_mobject = number_tex
         factor_counts = defaultdict(int)
         division_equations = []
         factor_mobjects = []
-        number_mobjects = [number_126]
+        number_mobjects = [number_tex]
 
         # Start decomposing
         while current_number != 1:
-            for prime in primes:
+            for prime in self.primes:
                 if current_number % prime == 0:
                     # Highlight the prime
-                    prime_index = primes.index(prime)
-                    prime_mobject = primes_group[prime_index]
+                    prime_index = self.primes.index(prime)
+                    prime_mobject = self.primes_group[prime_index]
                     rect = SurroundingRectangle(prime_mobject, color=RED)
                     self.play(Create(rect))
 
@@ -58,6 +79,7 @@ class PrimeFactorDecomposition(Scene):
                     factor.next_to(number_mobject, RIGHT, buff=0.5)
                     self.play(Write(factor))
                     factor_mobjects.append(factor)
+                    decomposition_group.add(factor)
 
                     # Write division equation on the right
                     new_number = current_number // prime
@@ -70,15 +92,16 @@ class PrimeFactorDecomposition(Scene):
                         )
                     else:
                         division_eq.to_edge(RIGHT).shift(UP).shift(LEFT * 2)
-
                     self.play(Write(division_eq))
                     division_equations.append(division_eq)
+                    # decomposition_group.add(division_eq)
 
                     # Place new number under the current one, aligned on the right
                     number_new = MathTex(f"{new_number}")
                     number_new.next_to(number_mobject, DOWN, aligned_edge=RIGHT)
                     self.play(TransformFromCopy(division_eq[-1], number_new))
                     number_mobjects.append(number_new)
+                    decomposition_group.add(number_new)
 
                     # Update current number and mobject
                     current_number = new_number
@@ -91,11 +114,13 @@ class PrimeFactorDecomposition(Scene):
             else:
                 # If no primes divide current_number, it is a prime itself
                 # Highlight the prime (if within our list)
-                if current_number in primes:
-                    prime_index = primes.index(current_number)
-                    prime_mobject = primes_group[prime_index]
+                if current_number in self.primes:
+                    prime_index = self.primes.index(current_number)
+                    prime_mobject = self.primes_group[prime_index]
                     rect = SurroundingRectangle(prime_mobject, color=RED)
                     self.play(Create(rect))
+                else:
+                    rect = None
 
                 # Update factor count
                 factor_counts[current_number] += 1
@@ -105,37 +130,45 @@ class PrimeFactorDecomposition(Scene):
                 factor.next_to(number_mobject, RIGHT, buff=0.5)
                 self.play(Write(factor))
                 factor_mobjects.append(factor)
+                decomposition_group.add(factor)
 
                 # Write division equation on the right
                 division_eq = MathTex(f"{current_number} \\div {current_number} = 1")
                 division_eq.next_to(division_equations[-1], DOWN, aligned_edge=RIGHT)
                 self.play(Write(division_eq))
                 division_equations.append(division_eq)
+                # decomposition_group.add(division_eq)
 
                 # Place number 1 under the current number, aligned on the right
                 number_new = MathTex("1")
                 number_new.next_to(number_mobject, DOWN, aligned_edge=RIGHT)
                 self.play(TransformFromCopy(division_eq[-1], number_new))
                 number_mobjects.append(number_new)
+                decomposition_group.add(number_new)
 
                 # Update current number and mobject
                 current_number = 1
                 number_mobject = number_new
 
                 # Fade out division equation and prime highlight
-                self.play(FadeOut(division_eq), FadeOut(rect))
+                self.play(FadeOut(division_eq))
+                if rect:
+                    self.play(FadeOut(rect))
 
         # Group all factor mobjects
         factors_group = VGroup(*factor_mobjects)
+        decomposition_group.add(factors_group)
 
         # Create a rectangle around all factors
         rect_around_factors = SurroundingRectangle(factors_group, color=BLUE)
         self.play(Create(rect_around_factors))
+        # decomposition_group.add(rect_around_factors)
 
-        # Build the initial expression "126 ="
-        initial_expression = MathTex("126", "=")
+        # Build the initial expression "{number} ="
+        initial_expression = MathTex(str(number), "=")
         initial_expression.to_edge(DOWN, buff=1).shift(LEFT * 1.5)
         self.play(Write(initial_expression))
+        # decomposition_group.add(initial_expression)
 
         # Initialize the expression VGroup with the initial expression
         expression = VGroup(*initial_expression)
@@ -161,8 +194,9 @@ class PrimeFactorDecomposition(Scene):
                 times.next_to(last_element, RIGHT, buff=0.2)
                 self.play(Write(times))
                 expression.add(times)
+                # decomposition_group.add(times)
 
-            # Position the factor copy next to the last element (times symbol or equals sign)
+            # Position the factor copy next to the last element
             if times:
                 factor_copy.next_to(times, RIGHT, buff=0.2)
             else:
@@ -173,57 +207,45 @@ class PrimeFactorDecomposition(Scene):
 
             # Add the factor to the expression VGroup
             expression.add(factor_copy)
+            # decomposition_group.add(factor_copy)
 
-        # Build the LaTeX string for the product of factors, isolating "3 \\times 3"
-        product_string = "126 = 2 \\times {3 \\times 3} \\times 7"
-        final_expression = MathTex(
-            product_string, substrings_to_isolate=["3 \\times 3"]
-        )
-
-        # final_expression.next_to(vertical_line, DOWN, buff=1)
-        final_expression.to_edge(DOWN, buff=1)
+        # Build the LaTeX string for the product of factors, isolating repeated factors
+        factor_strings = []
+        for prime, count in sorted(factor_counts.items()):
+            if count == 1:
+                factor_strings.append(f"{prime}")
+            else:
+                factor_strings.extend([f"{prime}"] * count)
+        product_string = f"{number} = " + " \\times ".join(factor_strings)
+        final_expression = MathTex(product_string)
+        final_expression.to_edge(DOWN, buff=1)  # .shift(LEFT * 1.5)
 
         self.play(FadeTransform(expression, final_expression))
+        decomposition_group.add(final_expression)
 
         # Transform factors into the final expression at the bottom
-        # Position the factors copy at the location of the final expression
-        factors_copy = factors_group.copy()
-        # factors_copy.generate_target()
-        # factors_copy.target.move_to(final_expression.get_center())
+        self.play(FadeOut(rect_around_factors))
 
-        # Move factors to the final expression position
-        # self.play(MoveToTarget(factors_copy))
-
-        # Transform factors into the final expression
-        self.play(
-            # TransformMatchingTex(factors_copy, final_expression),
-            FadeOut(rect_around_factors),
-        )
-
-        # Continue with the rectangle around "3 × 3" and the exponentiation
-        # Create a rectangle around "3 × 3" in the final expression
-        three_times_three = final_expression.get_part_by_tex("3 \\times 3")
-        rect_around_three_times_three = SurroundingRectangle(
-            three_times_three, color=YELLOW
-        )
-        self.play(Create(rect_around_three_times_three))
-
-        # Build the LaTeX string for the expression with exponents, isolating "3^{2}"
-        exponent_string = "126 = 2 \\times {3^{2}} \\times 7"
-        final_expression_with_powers = MathTex(
-            exponent_string, substrings_to_isolate=["3^{2}"]
-        )
+        # Build the LaTeX string for the expression with exponents
+        exponent_strings = []
+        for prime, count in sorted(factor_counts.items()):
+            if count == 1:
+                exponent_strings.append(f"{prime}")
+            else:
+                exponent_strings.append(f"{prime}^{{{count}}}")
+        exponent_string = f"{number} = " + " \\times ".join(exponent_strings)
+        final_expression_with_powers = MathTex(exponent_string)
         final_expression_with_powers.next_to(final_expression, DOWN, buff=0.3)
+        decomposition_group.add(final_expression_with_powers)
 
         # Transform the final expression to the one with exponents
         self.play(
-            TransformMatchingTex(
-                final_expression.copy(),
-                final_expression_with_powers,
-                path_alphas=[0, 1],
-                transform_mismatches=True,
-            ),
-            FadeOut(rect_around_three_times_three),
+            TransformMatchingTex(final_expression.copy(), final_expression_with_powers)
         )
 
-        self.wait(2)
+        self.wait(1)
+
+        # Shift the entire decomposition group
+        self.play(decomposition_group.animate.shift(shift_amount))
+
+        return decomposition_group
