3cc95425e331f3e6a9bb0acacb5edfefc86338d5
Makcime <marlier.maxime@gmail.com>
2025-01-22 20:55:05 +0100

    [add] grid and fix some positionning

--- scenes/vectors/addition.py
+++ scenes/vectors/addition.py
@@ -3,6 +3,10 @@ from manim import *
 
 class VectorExample(Scene):
     def construct(self):
+        # Create a plane with invisible axes but visible grid lines
+        plane = NumberPlane(
+            x_axis_config={"stroke_width": 0}, y_axis_config={"stroke_width": 0}
+        )
         # Define vectors and shift v1 to start at a different location
         # v1 = Vector([1, 2], color=BLUE).shift(RIGHT * 2 + UP * 1)
         v1 = Vector([1, 2], color=BLUE)
@@ -12,7 +16,7 @@ class VectorExample(Scene):
         # Create labels with arrow notation
         l1 = MathTex(r"\vec{v}_1", color=BLUE).move_to(v1.get_center() + RIGHT * 0.3)
         ml1 = MathTex(r"-\vec{v}_1", color=YELLOW).move_to(
-            mv1.get_center() + RIGHT * 0.3
+            mv1.get_center() + LEFT * 0.5
         )
         l2 = MathTex(r"\vec{v}_2", color=GREEN).move_to(v2.get_center() + UP * 0.3)
 
@@ -31,12 +35,12 @@ class VectorExample(Scene):
         # Define the diff vector and its label, placed in the middle
         vdiff = Vector([-1 - 5, -2 - 2], color=PURPLE)  # diff of the two vectors
         ldiff = MathTex(r"\vec{v}_2 - \vec{v}_1", color=PURPLE).move_to(
-            vdiff.get_center() + DOWN * 0.3
+            vdiff.get_center() + RIGHT
         )
         vdiffg = VGroup(vdiff, ldiff)
 
         # Add groups to the scene
-        self.add(v1g, v2g)
+        self.add(plane, v1g, v2g)
         self.wait(2)
 
         # Animate moving v2 so its tail attaches to the head of v1
@@ -45,6 +49,7 @@ class VectorExample(Scene):
         self.play(vsumg.animate.shift(v1.get_start()))
         self.wait(2)
 
+        l1.shift(LEFT)
         self.shift(v2, v1, v1g)
         self.wait(2)
 
@@ -53,6 +58,7 @@ class VectorExample(Scene):
 
         shift_amount = v2.get_start() - vsum.get_start()
         self.play(vsumg.animate.shift(shift_amount))
+        self.play(lsum.animate.shift(UP * 0.7))
 
         self.wait(2)
         mv1g.shift(v1.get_start())
